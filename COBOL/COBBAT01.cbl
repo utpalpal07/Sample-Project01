f737e7335ba0b8fed917b37c81a1cad3b4fe38db
utpalpal07 <67892716+utpalpal07@users.noreply.github.com>
2023-03-17 15:51:29 +0530

    Create COBBAT01.cbl

---
+++ COBOL/COBBAT01.cbl
@@ -0,0 +1,69 @@
+      *-----------------------                                          
+       IDENTIFICATION DIVISION.                                         
+      *-----------------------                                          
+       PROGRAM-ID.  COBBAT01.                                           
+      *--------------------                                             
+       ENVIRONMENT DIVISION.                                            
+      *--------------------                                             
+       INPUT-OUTPUT SECTION.                                            
+       FILE-CONTROL.                                                    
+           SELECT INP-INPUT  ASSIGN TO INDD.                            
+           SELECT OUT-OUTPUT ASSIGN TO OUTDD.                           
+      *-------------                                                    
+       DATA DIVISION.                                                   
+      *-------------                                                    
+       FILE SECTION.                                                    
+       FD  INP-INPUT  RECORDING MODE F.    
+       01  INPUT-REC.                                                   
+           05  INP-NAME       PIC X(10).                                
+           05  FILLER         PIC X(70).                                
+      *                                                                 
+       FD  OUT-OUTPUT RECORDING MODE F.                                 
+       01  OUTPUT-REC.                                                  
+           05  OUT-NAME       PIC X(10).                                
+           05  FILLER         PIC X(01) VALUE '-'.                      
+           05  CURR-DATE      PIC X(10).                                
+           05  FILLER         PIC X(59) VALUE SPACES.                   
+      *                                                                 
+       WORKING-STORAGE SECTION.                                         
+       01 FLAGS.                                                        
+         05 LASTREC           PIC X VALUE SPACE.                        
+      *                                                                 
+      *------------------                                               
+       PROCEDURE DIVISION.                                              
+      *------------------                                               
+      *                                                                 
+           DISPLAY 'PROGRAM COBBAT01 - START'.                          
+      *                                                                 
+       OPEN-FILES.                                                      
+           OPEN INPUT  INP-INPUT.                                       
+           OPEN OUTPUT OUT-OUTPUT.                                      
+      *                                                                 
+       READ-WRITE-UNTIL-LASTREC.                                        
+           PERFORM READ-INP-INPUT                                       
+           PERFORM UNTIL LASTREC = 'Y'                                  
+              PERFORM WRITE-COMBINED                                    
+              PERFORM READ-INP-INPUT                                    
+           END-PERFORM.                                                 
+      *                                                                 
+       CLOSE-STOP.                                                      
+      *                                                                 
+           DISPLAY 'PROGRAM COBBAT01 - STOP'.                           
+      *                                                                 
+           CLOSE INP-INPUT.                                             
+           CLOSE OUT-OUTPUT.                                            
+           GOBACK.                                                      
+      *                                                                 
+       READ-INP-INPUT.                                                  
+           INITIALIZE INPUT-REC.                                        
+      *                                                                 
+           READ INP-INPUT                                               
+           AT END MOVE 'Y' TO LASTREC                                   
+           END-READ.                                                    
+      *                                                                 
+       WRITE-COMBINED.                                                  
+           MOVE SPACES       TO  OUTPUT-REC                             
+           MOVE INP-NAME     TO  OUT-NAME                               
+           MOVE '14-11-2022' TO  CURR-DATE                              
+           WRITE OUTPUT-REC.                                            
+      *                                                                 
